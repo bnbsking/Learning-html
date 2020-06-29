a9eb8ade12c96c8cedc0f130c26d9dfe3f83908d
Bobo <bnbsking@gmail.com>
2020-06-29 11:40:17 +0800

    Add files via upload

---
+++ three_buddy/js.js
@@ -0,0 +1,11 @@
+function myFunction() {
+	if(x==0){
+		document.getElementById("demo").innerHTML = "Paragraph changed.";
+		x=1;	
+	}
+	else{
+		document.getElementById("demo").innerHTML = "A Paragraph.";
+		x=0;
+	}
+	//alert(x);
+}
